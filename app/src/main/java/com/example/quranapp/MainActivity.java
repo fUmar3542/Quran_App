2f4343ca21329ecf253af96405f01e578c7fd435
fumar3542 <fumar3542@outlook.com>
2022-12-06 17:07:28 +0500

    Font binded successfully

--- app/src/main/java/com/example/quranapp/MainActivity.java
+++ app/src/main/java/com/example/quranapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.quranapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                TextView t = (TextView)findViewById(R.id.ayat);
+                t.setText("Yes");
+            }
+        });
     }
+
+
+
+
+
 }
